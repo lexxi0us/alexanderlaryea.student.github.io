c34246f1eb18b26e27062b21afef6ced449e586a
lexxi0us <alexaniilaryea@yahoo.com>
2024-07-11 16:52:46 -0400

    Update basicmap.js

--- basicmap.js
+++ basicmap.js
@@ -3,8 +3,8 @@ console.log("basicmap.jsloaded.");
 //CPCC cordinates: 35.2176665 N, 809.831473 W-> 35.2176665, -80.831473
 
 let map = L.map("map", {
-  center : [35.2176665, -80.831473],
-  zoom : 15
+  center: [35.2176665, -80.831473],
+  zoom: 15
 });
 
 L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
