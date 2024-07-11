5dca60b036402974fd1a0702bbca72af61a20de5
lexxi0us <alexaniilaryea@yahoo.com>
2024-07-11 16:53:54 -0400

    Update basicmap.js

--- basicmap.js
+++ basicmap.js
@@ -11,3 +11,5 @@ L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
     maxZoom: 19,
     attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
 }).addTo(map);
+
+console.log("basicmap.jsloaded again.");
