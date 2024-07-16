60ff361d0bb88656f853b4f6f7923313433d39b4
lexxi0us <alexaniilaryea@yahoo.com>
2024-07-16 15:56:03 -0400

    Update basicmap.js

--- basicmap.js
+++ basicmap.js
@@ -11,8 +11,10 @@ L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
     maxZoom: 19,
     attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
 }).addTo(map);
-L.marker(35.2176665, -80.831473), {
-  title"Hello from cpcc"
-}).addTo(map) 
+
+L.marker([35.2176665, -80.831473], {
+  title: "Hello from cpcc"
+}).addTo(map);
+
 console.log("basicmap.jsloaded again.");
 
