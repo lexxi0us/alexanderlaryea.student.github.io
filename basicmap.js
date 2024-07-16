44f04bb0d894429e0da1dec89204c6b788a2e752
lexxi0us <alexaniilaryea@yahoo.com>
2024-07-16 16:35:15 -0400

    Update basicmap.js

--- basicmap.js
+++ basicmap.js
@@ -19,7 +19,7 @@ L.marker([35.2176665, -80.831473], {
 <h2>Hello from cpcc</h2>
 <hr>
 <a href="https://www.cpcc.edu">Click here to visit CPCC's Website</a><br>
-<iframe width="560" height="315" src="https://www.youtube.com/embed/wCyHWiYPiXU?si=KLdReNWLNeIBd4wt" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
+<iframe width="260" height="120" src="https://www.youtube.com/embed/wCyHWiYPiXU?si=KLdReNWLNeIBd4wt" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
 </center>
 `)
 }).addTo(map);
