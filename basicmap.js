d8cec9aa4e0095896309d5245de46d2330e5e553
lexxi0us <alexaniilaryea@yahoo.com>
2024-07-18 15:47:22 -0400

    Update basicmap.js

--- basicmap.js
+++ basicmap.js
@@ -2,12 +2,6 @@ console.log("basicmap.jsloaded.");
 
 //CPCC cordinates: 35.2176665 N, 809.831473 W-> 35.2176665, -80.831473
 
-let map = L.map("map", {
-  center: [35.2176665, -80.831473],
-  zoom: 15
-});
-
-
 
 let makewrs =[];
 
